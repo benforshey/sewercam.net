841e4a9ed129b15b8204a57136b6e520c5a716b7
Ben Forshey <hello@integrisweb.com>
2018-01-10 13:13:46 -0500

    remove appcache; update service area

--- gulpfile.js
+++ gulpfile.js
@@ -16,17 +16,17 @@ gulp.task('js', function () {
   .pipe(gulp.dest('./scripts/dist/'))
 })
 
-// gulp.task('css', function () {
-//   var plugins = [
-//     autoprefixer({browsers: ['last 3 version']}),
-//     cssnano()
-//   ]
-//   return gulp.src('./css/*.css')
-//   .pipe(sourcemaps.init())
-//   .pipe(postcss(plugins))
-//   .pipe(sourcemaps.write('./'))
-//   .pipe(gulp.dest('./css/dist/'))
-// })
+gulp.task('css', function () {
+  var plugins = [
+    autoprefixer({browsers: ['last 3 version']}),
+    cssnano()
+  ]
+  return gulp.src('./css/*.css')
+  .pipe(sourcemaps.init())
+  .pipe(postcss(plugins))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./css/dist/'))
+})
 
-// gulp.task('default', ['js', 'css'])
-gulp.task('default', ['js'])
+gulp.task('default', ['js', 'css'])
+// gulp.task('default', ['js'])
