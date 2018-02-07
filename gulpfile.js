128224874276c2259ec98786b5ae7252b113c2d0
Ben Forshey <hello@integrisweb.com>
2018-02-07 15:34:08 -0500

    move to zeit's now messaging server

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,32 @@
-const gulp = require('gulp')
-const minify = require('gulp-minify')
-const postcss = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
-const autoprefixer = require('autoprefixer')
-const cssnano = require('cssnano')
+const gulp = require('gulp');
+const minify = require('gulp-minify');
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
 
-gulp.task('js', function () {
+gulp.task('js', () => {
   gulp.src('./scripts/*.js')
-  .pipe(minify({
-    ext: {
-      src: '.js',
-      min: '-min.js'
-    }
-  }))
-  .pipe(gulp.dest('./scripts/dist/'))
-})
+    .pipe(minify({
+      ext: {
+        src: '.js',
+        min: '-min.js',
+      },
+    }))
+    .pipe(gulp.dest('./scripts/dist/'));
+});
 
-gulp.task('css', function () {
-  var plugins = [
-    autoprefixer({browsers: ['last 3 version']}),
-    cssnano()
-  ]
+gulp.task('css', () => {
+  const plugins = [
+    autoprefixer({ browsers: ['last 3 version'] }),
+    cssnano(),
+  ];
   return gulp.src('./css/*.css')
-  .pipe(sourcemaps.init())
-  .pipe(postcss(plugins))
-  .pipe(sourcemaps.write('./'))
-  .pipe(gulp.dest('./css/dist/'))
-})
+    .pipe(sourcemaps.init())
+    .pipe(postcss(plugins))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css/dist/'));
+});
 
-gulp.task('default', ['js', 'css'])
+gulp.task('default', ['js', 'css']);
 // gulp.task('default', ['js'])
--- scripts/dist/main.js
+++ scripts/dist/main.js
@@ -1,151 +1,146 @@
 /* eslint-env browser */
 /* global ga */
-function ready (fn) {
+function ready(fn) {
   if (document.readyState !== 'loading') {
-    fn()
+    fn();
   } else if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', fn)
+    document.addEventListener('DOMContentLoaded', fn);
   } else {
-    document.attachEvent('onreadystatechange', function () {
+    document.attachEvent('onreadystatechange', () => {
       if (document.readyState !== 'loading') {
-        fn()
+        fn();
       }
-    })
+    });
   }
 }
 
-var readyWrap = function () {
-  'use strict'
+function readyWrap() {
+  const button = document.getElementsByClassName('form_submit')[0];
+  const feedbackContainer = document.getElementsByClassName('form_feedback');
+  const inputs = document.getElementsByTagName('input');
+  const inspectionForm = document.getElementsByClassName('inspection_form');
+  const logoNav = document.getElementsByClassName('logoNav');
+  const security = document.getElementsByClassName('form_security');
 
-  var button = document.getElementsByClassName('form_submit')[0]
-  var feedbackContainer = document.getElementsByClassName('form_feedback')
-  var inputs = document.getElementsByTagName('input')
-  var inspectionForm = document.getElementsByClassName('inspection_form')
-  var logoNav = document.getElementsByClassName('logoNav')
-  var security = document.getElementsByClassName('form_security')
-
-   // Register the ServiceWorker.
+  // Register the ServiceWorker.
   if ('serviceWorker' in navigator) {
     // The service worker cannot access parent directories (apart from explicity setting scope), so keep it in the root directory.
-    navigator.serviceWorker.register('serviceWorker.js').then(function (registration) {
-      console.log(`ServiceWorker registration successful with scope: ${registration.scope}`)
-    }).catch(function (e) {
-      console.log(`ServiceWorker registration failed: ${e}`)
-    })
+    navigator.serviceWorker.register('serviceWorker.js').then((registration) => {
+      console.log(`ServiceWorker registration successful with scope: ${registration.scope}`);
+    }).catch((e) => {
+      console.log(`ServiceWorker registration failed: ${e}`);
+    });
   }
 
-  function inputWidthController (target) {
-    var base = 25 // base size for all inputs
-    var chars = target.value.length
-    var size = target.size
+  function inputWidthController(target) {
+    const base = 25; // base size for all inputs
+    const chars = target.value.length;
+    const size = target.size;
     if (chars > size) { // if the input needs to expand
-      target.size = target.value.length // expand it
-      return target.size
+      target.size = target.value.length; // expand it
+      return target.size;
     } else if (chars < size && chars > base) { // if the input contains less than its width and still has more than 15 characters
-      target.size = target.value.length // shrink it
-      return target.size
-    } else { // otherwise
-      target.size = base // set to the base width
-      return target.size
-    }
+      target.size = target.value.length; // shrink it
+      return target.size;
+    } // otherwise
+    target.size = base; // set to the base width
+    return target.size;
   }
 
-  function sendReport () {
-    var AJAX = new XMLHttpRequest()
-    var feedback = document.createElement('p')
-    var payload = new FormData(inspectionForm[0])
-    var URL = 'https://sewercam.net/webhook/form-sms/'
+  function sendReport() {
+    const AJAX = new XMLHttpRequest();
+    const feedback = document.createElement('p');
+    const payload = new FormData(inspectionForm[0]);
+    const URL = 'https://message.integrisweb.com/sms/';
 
     if (security[0].value) { // if the security input has a value, it's been filled
-      feedback.className = 'feedback_error'
-      feedback.innerHTML = "Sorry for the inconvenience, but you somehow triggered our anti-spam protection. Please use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us."
-      feedbackContainer[0].appendChild(feedback)
+      feedback.className = 'feedback_error';
+      feedback.innerHTML = "Sorry for the inconvenience, but you somehow triggered our anti-spam protection. Please use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us.";
+      feedbackContainer[0].appendChild(feedback);
     } else {
       if (navigator.onLine === false) {
-        feedback.className = 'feedback_warning'
-        feedback.innerHTML = "Sorry for the inconvenience, but you are currently offline. You can't send a message while offline. You can wait until you are back online or use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us."
-        feedbackContainer[0].appendChild(feedback)
+        feedback.className = 'feedback_warning';
+        feedback.innerHTML = "Sorry for the inconvenience, but you are currently offline. You can't send a message while offline. You can wait until you are back online or use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us.";
+        feedbackContainer[0].appendChild(feedback);
       }
 
-      AJAX.addEventListener('load', function (e) {
+      AJAX.addEventListener('load', (e) => {
         if (e.target.status === 200) { // if the message was sent
-          feedback.className = 'feedback_success'
-          feedback.innerHTML = 'Your message was sent!'
-          feedbackContainer[0].appendChild(feedback)
+          feedback.className = 'feedback_success';
+          feedback.innerHTML = 'Your message was sent!';
+          feedbackContainer[0].appendChild(feedback);
 
-          ga('send', {  // since GA is loaded in the head, assume its presence
+          ga('send', { // since GA is loaded in the head, assume its presence
             hitType: 'event',
             eventCategory: 'Form',
             eventAction: 'send',
-            eventLabel: 'Contact Form'
-          })
+            eventLabel: 'Contact Form',
+          });
 
-          document.querySelector('.inspection_form').reset()  // empty the form on success
+          document.querySelector('.inspection_form').reset(); // empty the form on success
         } else { // something nonspecific has gone wrong
-          feedback.className = 'feedback_warning'
-          feedback.innerHTML = "Sorry for the inconvenience, but your message may have not sent. You can try sending it again or use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us."
-          feedbackContainer[0].appendChild(feedback)
+          feedback.className = 'feedback_warning';
+          feedback.innerHTML = "Sorry for the inconvenience, but your message may have not sent. You can try sending it again or use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us.";
+          feedbackContainer[0].appendChild(feedback);
         }
-      })
+      });
 
-      AJAX.open('POST', URL, true)
-      AJAX.send(payload)
+      AJAX.open('POST', URL, true);
+      AJAX.send(payload);
     }
   }
 
-  function formConnectionManager () {
+  function formConnectionManager() {
     if (navigator.onLine === false) {
-      var feedback = document.createElement('p')
+      const feedback = document.createElement('p');
 
-      button.setAttribute('disabled', 'true')
-      button.className = 'button form_submit disabled'
+      button.setAttribute('disabled', 'true');
+      button.className = 'button form_submit disabled';
 
-      feedback.className = 'feedback_warning'
-      feedback.innerHTML = "Sorry for the inconvenience, but you are currently offline. You can't send a message while offline. You can wait until you are back online or use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us."
-      return feedbackContainer[0].appendChild(feedback)
-    } else {
-      while (feedbackContainer[0].hasChildNodes()) {
-        feedbackContainer[0].removeChild(feedbackContainer[0].firstChild)
-      }
-
-      button.removeAttribute('disabled')
-      button.className = 'button form_submit'
-      return button.className
+      feedback.className = 'feedback_warning';
+      feedback.innerHTML = "Sorry for the inconvenience, but you are currently offline. You can't send a message while offline. You can wait until you are back online or use the contact information at the <a data-scroll href='#footer'>bottom of the page</a> to reach us.";
+      return feedbackContainer[0].appendChild(feedback);
     }
+    while (feedbackContainer[0].hasChildNodes()) {
+      feedbackContainer[0].removeChild(feedbackContainer[0].firstChild);
+    }
+
+    button.removeAttribute('disabled');
+    button.className = 'button form_submit';
+    return button.className;
   }
 
   /* event listeners & function calls */
 
   if (logoNav.length) {
-    logoNav[0].addEventListener('click', function () {
-      window.location = 'https://sewercam.net/'
-    })
+    logoNav[0].addEventListener('click', () => {
+      window.location = 'https://sewercam.net/';
+    });
   }
 
   if (inputs.length) { // the only page with a form is 'your-sewer-scope.html'
-
-    for (var i = 0; i < inputs.length; i++) {
-      inputs[i].addEventListener('input', function (e) {
-        inputWidthController(e.target)
-      })
+    for (let i = 0; i < inputs.length; i += 1) {
+      inputs[i].addEventListener('input', (e) => {
+        inputWidthController(e.target);
+      });
     }
 
-    inspectionForm[0].addEventListener('submit', function (e) {
-      e.preventDefault()
-      return sendReport()
-    })
+    inspectionForm[0].addEventListener('submit', (e) => {
+      e.preventDefault();
+      return sendReport();
+    });
 
-    window.addEventListener('online', formConnectionManager)
+    window.addEventListener('online', formConnectionManager);
 
-    window.addEventListener('offline', formConnectionManager)
+    window.addEventListener('offline', formConnectionManager);
   }
 
   if (typeof smoothScroll !== 'undefined') { // check if smoothScroll script is present
     smoothScroll.init({ // init smooth scroll
       easing: 'easeInOutQuad',
-      speed: 500
-    })
+      speed: 500,
+    });
   }
 }
 
-ready(readyWrap)
+ready(readyWrap);
