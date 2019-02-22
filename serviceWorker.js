6a6b815667c78a0a07d7d482c6f8e10a3122f0cf
Ben Forshey <ben@integrisweb.com>
2019-02-22 12:20:30 -0500

    version service worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,6 +1,6 @@
 /* eslint-env worker */
 const cacheName = 'Offline First'
-const cacheVersion = '1.0.6'
+const cacheVersion = '1.0.7'
 // String of pages & assets relative to project root.
 // If any file in this list fails, the whole service worker fails to install.
 const cacheURIs = [
