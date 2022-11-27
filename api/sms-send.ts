2e4eb73f90daed34750c73f14732166b1bdfd9ff
Ben Forshey <ben@integrisweb.com>
2022-11-27 16:06:30 -0500

    Add Code Formatting; Switch to Plivo

--- api/sms-send.ts
+++ api/sms-send.ts
@@ -1,22 +1,25 @@
-import dotenv from "dotenv";
 import { NowRequest, NowResponse } from "@now/node";
+import dotenv from "dotenv";
+import { PhloClient } from "plivo";
 
 dotenv.config();
 
-const client = require("twilio")(
-  process.env.TWILIO_SID,
-  process.env.TWILIO_AUTH
+const client = new PhloClient(
+  process.env.PLIVO_AUTH_ID,
+  process.env.PLIVO_AUTH_TOKEN,
+  {}
 );
 
 export default (req: NowRequest, res: NowResponse) => {
   try {
     const { address, contact, name, time } = req.body;
 
-    return client.messages
-      .create({
-        body: `${name} @ ${contact} for ${address} on ${time}`,
-        from: process.env.TWILIO_NUMBER,
-        to: process.env.CLIENT_NUMBER,
+    return client
+      .phlo(process.env.PHLO_ID)
+      .run({
+        from: process.env.FROM_NUMBER,
+        to: process.env.TO_NUMBER,
+        message: `${name} @ ${contact} for ${address} on ${time}.`,
       })
       .then(() => res.status(200).end())
       .catch((error: Error) => res.status(400).end(JSON.parse(error.message)));
