d47ed61df677e969dd4f5933764a7b5f4537e4ba
Ben Forshey <ben@integrisweb.com>
2023-03-06 18:59:54 -0500

    Prevent 504

--- api/email-send.ts
+++ api/email-send.ts
@@ -12,16 +12,16 @@ export default (req: NowRequest, res: NowResponse) => {
   try {
     const { address, contact, name, time } = req.body;
 
-    client
-      .sendEmail({
-        From: "no-reply@sewercam.net",
-        To: process.env.TO_EMAIL,
-        Subject: "Sewercam Booking Request",
-        HtmlBody: `${name} @ ${contact} for ${address} on ${time}.`,
-        TextBody: `${name} @ ${contact} for ${address} on ${time}.`,
-        MessageStream: "outbound",
-      })
-      .catch((error: Error) => res.status(400).end(error));
+    client.sendEmail({
+      From: "no-reply@sewercam.net",
+      To: process.env.TO_EMAIL,
+      Subject: "Sewercam Booking Request",
+      HtmlBody: `${name} @ ${contact} for ${address} on ${time}.`,
+      TextBody: `${name} @ ${contact} for ${address} on ${time}.`,
+      MessageStream: "outbound",
+    });
+
+    return res.status(200).json({ status: 200, time: Date.now() });
   } catch (error) {
     return res.status(400).end(error);
   }
